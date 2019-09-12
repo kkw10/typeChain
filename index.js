ccb4558a6ed42a0cd66251a6c159fcf9e77e1a5b
kkw_10 <kkw_10@naver.com>
2019-09-12 22:48:48 +0900

    기능: 간단한 연습 코드 작성

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Hello TypeScript");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Koon";
+const age = 28;
+const gender = "male";
+const sayHello = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHello(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello TypeScript")
+const name = "Koon";
+const age = 28;
+const gender = "male";
+
+const sayHello = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHello(name, age);
+
+export {};
+
