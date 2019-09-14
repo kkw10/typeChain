83e8b55219cb49587382883818167a658faa05c4
kkw_10 <kkw_10@naver.com>
2019-09-14 16:55:34 +0900

    기능: class 사용해보기

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-interface Human {
-    name: string,
-    gender: string,
-    age: number
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender?: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: 'Koon',
-    gender: 'male',
-    age: 29
-}
+const Koon = new Human("Koon", 29, "male");
 
 const sayHello = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
 }
 
-console.log(sayHello(person));
+console.log(sayHello(Koon));
 
 export {};
 
