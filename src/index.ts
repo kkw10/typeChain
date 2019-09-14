78e2a2104e14153769712475fc562ed89eda4edd
kkw_10 <kkw_10@naver.com>
2019-09-14 18:04:50 +0900

    기능: 블럭 정의 및 생성

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender?: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public prevHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor (
+        index: number,
+        hash: string,
+        prevHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.prevHash = prevHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const Koon = new Human("Koon", 29, "male");
+const genesisBlock:Block = new Block(0, "1991", "", "Hello", 123456);
 
-const sayHello = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
-}
+let blockChain: [Block] = [genesisBlock];
 
-console.log(sayHello(Koon));
+console.log(blockChain);
 
 export {};
 
