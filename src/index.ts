965f122cd9bbb7ff28f71552353814cd0addba9c
kkw_10 <kkw_10@naver.com>
2019-09-14 21:20:38 +0900

    기능: 새 블럭만드는 기능 구현

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,69 @@
+import * as CryptoJs from 'crypto-js';
+
 class Block {
     public index: number;
     public hash: string;
     public prevHash: string;
     public data: string;
-    public timestamp: number;
+    public timeStamp: number;
+
+    static calculateBlockHash = (
+        index: number,
+        prevHash: string,
+        timeStamp: number,
+        data: string
+    ): string => CryptoJs.SHA256(index + prevHash + timeStamp + data).toString();
+
     constructor (
         index: number,
         hash: string,
         prevHash: string,
         data: string,
-        timestamp: number
+        timeStamp: number
     ) {
         this.index = index;
         this.hash = hash;
         this.prevHash = prevHash;
         this.data = data;
-        this.timestamp = timestamp;
+        this.timeStamp = timeStamp;
     }
 }
 
+
+
 const genesisBlock:Block = new Block(0, "1991", "", "Hello", 123456);
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
 
-console.log(blockChain);
+const getBlockchain = (): Block[] => blockChain;
+
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+    const prevBlock: Block = getLatestBlock();
+    const newIndex: number = prevBlock.index + 1;
+    const newTimeStamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(
+        newIndex,
+        prevBlock.hash,
+        newTimeStamp,
+        data
+    );
+
+    const newBlock: Block = new Block(
+        newIndex,
+        newHash,
+        prevBlock.hash,
+        data,
+        newTimeStamp
+    )
+
+    return newBlock;
+}
+
+console.log(createNewBlock("Hello"), createNewBlock("Bye"));
 
 export {};
 
